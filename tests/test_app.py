b92c47d07ae3009d6d54779b9b2d8ed30ac46656
ignaciogros <ignacio@gros.es>
2026-06-17 19:27:59 +0200

    Mejora de las pruebas automatizadas.

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,12 @@ def test_root():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_root_html_content():
+    client = app.test_client()
+    response = client.get("/")
+    html = response.data.decode("utf-8")
+    assert "<!DOCTYPE html>" in html
+    assert "Entregable 4" in html
+    assert "¡Hola!" in html
